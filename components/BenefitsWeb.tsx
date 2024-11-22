bff916a4d13d7dc9a6d2a0dd18cc396508520437
Manuel-latorre <manuel.latorre11@gmail.com>
2024-11-22 12:33:33 -0300

    missing key

--- components/BenefitsWeb.tsx
+++ components/BenefitsWeb.tsx
@@ -86,7 +86,7 @@ const BenefitsWeb = () => {
     <div className='max-lg:hidden w-full 2xl:w-[60%] mx-auto flex flex-wrap justify-center gap-4 px-2 mt-12'>
         {
             benefits.map((data) => (
-                <div className='p4 rounded-xl bgDark max-w-[250px] max-h-[200px] 2xl:h-[200px] flex flex-col justify-center gap-4 items-center text-center p-4 hover:scale-105 transition-all'>
+                <div key={data.name} className='p4 rounded-xl bgDark max-w-[250px] max-h-[200px] 2xl:h-[200px] flex flex-col justify-center gap-4 items-center text-center p-4 hover:scale-105 transition-all'>
                     {data.icon}
                     <p className='textSoft font-medium text-xl'>{data.name}</p>
                 </div>
